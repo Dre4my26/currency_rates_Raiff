425d98fe6fb6f0812e0ffc904b45647dd9a45f79
Nesterov Vladislav <dreamyguy26@gmail.com>
2022-09-28 16:17:07 +0300

    ez pz

---
+++ exchange_rate.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def main(currency='Евро'):
+    url = 'https://www.raiffeisen.ru/currency_rates/'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    text = soup.get_text()
+    splitted_text = text.split()
+    counter = 0
+    for key in splitted_text:
+        counter += 1
+        if key == currency:
+            selling_price = splitted_text[counter]
+            selling_price = float(selling_price)
+            selling_price = round(selling_price, 2)
+            print("Цена покупки:", selling_price)
+            break
+    return selling_price
+
+
+if __name__ == "__main__":
+    main()
